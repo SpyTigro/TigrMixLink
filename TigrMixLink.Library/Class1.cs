a9e6dc58e17e5e4de7c90ae4da12250284b738b7
Robbe Wuyts <robbe.wuyts06@gmail.com>
2025-04-06 16:44:29 +0200

    project setup

---
+++ TigrMixLink.Library/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TigrMixLink.Library
+{
+    public static class Class1
+    {
+        public static string Foo()
+        {
+            return "123";
+        }
+    }
+}
+
