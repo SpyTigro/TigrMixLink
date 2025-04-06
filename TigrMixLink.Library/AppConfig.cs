fc0dcb9b6e0795c662a771653517b2ed7c55d719
Robbe Wuyts <robbe.wuyts06@gmail.com>
2025-04-07 01:31:41 +0200

    added config file read support, editing through app will follow

---
+++ TigrMixLink.Library/AppConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TigrMixLink.Library
+{
+    public class AppConfig
+    {
+        public string comPort { get; set; }
+        public int baudRate { get; set; }  
+        public List<string> Devices { get; set; } 
+    }
+}
+
--- TigrMixLink/Form1.cs
+++ TigrMixLink/Form1.cs
@@ -2,13 +2,18 @@ using System;
 using System.CodeDom;
 using System.Collections.Generic;
 using System.Drawing;
-using System.IO;
+using System.IO.Ports;
 using System.Reflection;
 using System.Threading;
 using System.Windows.Forms;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Primitives;
+
+using AudioSwitcher.AudioApi.CoreAudio;
+using TigrMixLink.Library;
+using System.Text.Json;
+using AudioSwitcher.AudioApi;
 
 namespace TigrMixLink
 {
@@ -31,7 +36,20 @@ namespace TigrMixLink
         }
 
         #endregion
+        private AppConfig config;
+        private List<string> deviceNames;
+
+
+        private readonly CoreAudioController controller = new();
+        private IEnumerable<CoreAudioDevice> devices;
+        
+
         private int numVolumes = 0;
+
+        private SerialPort serialPort;
+        private string comPort = "COM5";
+        private int baud = 115200;
+
 
         public readonly IConfiguration Configuration;
 
@@ -43,6 +61,20 @@ namespace TigrMixLink
             Configuration = Program.Services!.GetRequiredService<IConfiguration>();
             ChangeToken.OnChange(() => Configuration.GetReloadToken(), OnChange);
             OnChange();
+
+
+            if (File.Exists("config.json"))
+            {
+                String json = File.ReadAllText("config.json");
+                config = JsonSerializer.Deserialize<AppConfig>(json);
+                comPort = config.comPort;
+                baud = config.baudRate;
+                deviceNames = config.Devices;
+            }
+            serialPort = new(comPort, baud);
+            serialPort.DataReceived += SerialPort_DataReceived;
+            serialPort.Open();
+
         }
 
         private void OnChange()
@@ -57,16 +89,62 @@ namespace TigrMixLink
 
         private void addVolumeController()
         {
-            ComboBox comboBox = new();
-            comboBox.Size = new(100, 20);
-            comboBox.Location = new(40 + numVolumes * 120, 50);
+            ComboBox comboBox = new()
+            {
+                Size = new(100, 20),
+                Location = new(40 + numVolumes * 145, 50),
+                Name = "comboBox" + numVolumes.ToString()
+            };
+
+            Button remove = new()
+            {
+                Size = new(25, 25),
+                Location = new(150 + numVolumes * 145, 50),
+                Text = "-",
+                Name = "remove" + numVolumes.ToString(),
+                BackColor = Color.Red
+            };
+            remove.Click += (s, e) =>
+            {
+                int index = int.Parse(remove.Name.Substring(6));
+                Controls.Remove(comboBox);
+                Controls.Remove(remove);
+                for (int i = index; i < numVolumes-1; i++)
+                {
+                    if (Controls["comboBox" + (i + 1).ToString()] == null) break;
+                    Controls["comboBox" + (i + 1).ToString()].Name = "comboBox" + i.ToString();
+                    Controls["remove" + (i + 1).ToString()].Name = "remove" + i.ToString();
+                    Controls["comboBox" + i.ToString()].Location = new(40 + i * 145, 50);
+                    Controls["remove" + i.ToString()].Location = new(150 + i * 145, 50);
+                }
+                numVolumes--;
+            };
+
             numVolumes++;
             Controls.Add(comboBox);
+            Controls.Add(remove);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            devices = controller.GetDevices();
             addVolumeController();
+        }
+
+        private string prevInput= "";
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            String input = serialPort.ReadExisting();
+            if (input != prevInput) UpdateVolumes(input.Split('|'));
+            prevInput = input;
+        }
+
+        private void UpdateVolumes(String[] volumes)
+        {
+            for (int i = 0; i < Math.Min(volumes.Length, deviceNames.Count); i++) {
+                CoreAudioDevice device = devices.FirstOrDefault(d => d.FullName == deviceNames[i]);
+                device.Volume = int.Parse(volumes[i]);
+            }
         }
     }
 }
